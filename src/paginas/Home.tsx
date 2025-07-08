374b3cc904337fb1a81bb0c928637b78ea6738e4
Jemisse <macuacuajemisse@gmail.com>
2025-07-08 18:35:13 +0200

    HomeUpdate

--- src/paginas/Home.tsx
+++ src/paginas/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return(
-        <><TypographyH1 /></>
+        <><TypographyH1 /><InputDemo /></>
     )
 }
 
@@ -10,30 +10,57 @@ export default Home;
 export function TypographyH1() {
   return (
     
-    <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance text-sky-400">
-        <br />
-      Seja Bem Vindo a Lib-UJC, O seu Repositório Acadêmico.
-    </h1>
+    <><h5 className="scroll-m-20 text-center text-1xl font-extrabold tracking-tight text-balance text-sky-400">
+      <br />
+      Seja Bem Vindo a Lib-UJC,  O seu Repositório Acadêmico
+    </h5><p className="text-muted-foreground text-sm text-center">Feito por estudantes para estudantes.</p></>
+
   )
 }
 
 
-/*Como uma aplicação React, a pagina Home serve como ponto de entrada para a aplicação. 
-Ele renderiza o componente HomeHeader que sera aqui abaixo criado que sera o responsável
- por exibir o cabeçalho da página inicial. So tera um unico link (Faculdades no HomeHeader) como ponto de entrada para a 
-aplicação.*/
+import { Input } from "@/components/ui/input"
 
-//Tera um pequeno <H> de boas vindas a UNILIB uma <P texto> 
-// Futuramente essa primeira pagina tambem sera responsavel por exibir os eventos da UNILIB.
+export function InputDemo() {
+  return ( <div className="p-6">
+      <Input 
+        type="email" 
+        placeholder="Encontre o teu material" 
+        className="w-full max-w-sm"
+      />
+    </div>
+  )
+} 
 
-//Depois de escolher a faculdade, o usuario sera levado a pagina de Meterial (Onde tera acesso as restantes funcionaides 
-// do APlicativo).
-//  Ex: Bem vindo a UiniLib-UJC (A pagina material tera uma barra de pesquisa e os materias da faculdade selecionada,)
 
-//Penso que o footer podera ser colocado somente nesta pagina(Home.tsx) e na paina Sobre.tsx,
-// Que seja um footer simples, somente com copyright e o nome da UNILIB. e redes sociais.(Linkdin, Talvez E-mail ou Whatsapp)
-// Ou pode ser colocado somente na pagina Sobre.tsx.
 
-//Apenas anotando as ideias para.
-//Creio que so dara para lancar no Quarta Feira. Visto que hoje ja e domingo.
-//Nao coloquei links, usei testes unitarios para testas as  paginas ja "criadas".
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+export function SelectDemo() {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
+
