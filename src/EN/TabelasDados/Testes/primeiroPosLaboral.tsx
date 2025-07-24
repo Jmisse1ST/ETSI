cbf47a608fc812eafba64541aedf6768960b5525
Jemisse <macuacuajemisse@gmail.com>
2025-07-24 13:05:10 +0200

    TEBELA testes, primeiro, segundo, terceiro, quarto ano EN.

---
+++ src/EN/TabelasDados/Testes/primeiroPosLaboral.tsx
@@ -0,0 +1,76 @@
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const cadeira = [
+  {
+    cadeira: "-",
+    teste1: "Sem data",
+    teste2: "Sem data",
+  },
+  {
+    cadeira: "-",
+    teste1: "Sem data",
+    teste2: "Sem data",
+  },
+  {
+    cadeira: "-",
+    teste1: "Sem data",
+    teste2: "Sem data",
+  },
+  {
+    cadeira: "-",
+    teste1: "Sem data",
+    teste2: "Sem data",
+  },
+  {
+    cadeira: "-",
+    teste1: "Sem data",
+    teste2: "Sem data",
+  },
+  {
+    cadeira: "-",
+    teste1: "Sem data",
+    teste2: "Sem data",
+
+  },
+  
+]
+
+function PrimeiroPosLaboral() {
+  return (
+    <Table>
+      <TableCaption>POS-LABORAL : Sala Anfiteatro-1</TableCaption>
+      <TableHeader className="">
+        <TableRow>
+          <TableHead className="w-[100px] text-sky-400">Cadeiras</TableHead>
+          <TableHead className="text-sky-400">Teste - 1</TableHead>
+          <TableHead className="text-sky-400">Teste - 2</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {cadeira.map((cadeira) => (
+          <TableRow key={cadeira.cadeira}>
+            <TableCell className="font-medium ">{cadeira.cadeira}</TableCell>
+            <TableCell>{cadeira.teste1}</TableCell>
+            <TableCell>{cadeira.teste2}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  )
+}
+
+export default PrimeiroPosLaboral;
